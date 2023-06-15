be3b6d03346776e2bf40d762e4088df2f930e8f1
EnockYator <ekyator02@gmail.com>
2023-06-15 12:47:32 -0700

    Print out a string using puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ *File_name:4-puts.c
+ *Author:Enock Yator
+ *Decription:Print exactly "Programming is like building a multilingual puzzle
+ */
+
+#include <stdio.h>
+
+/**
+ *Main funtion
+ *Prints a string using puts() function
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+    return 0;
+}
