1b0a39b993214836bfe058eea9d5f43da2229909
EnockYator <ekyator02@gmail.com>
2023-06-15 13:34:16 -0700

    Print a string using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,11 @@
 #include <stdio.h>
 
 /**
- *Main funtion
- *Prints a string using puts() function
+ *main - main block
+ *Rturn:0
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
