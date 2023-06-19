84771a2566fd5a73452869c702eee4bcb8a5ea99
EnockYator <ekyator02@gmail.com>
2023-06-19 10:26:19 -0700

    Print all possible combination of 2 digits\ followed by comma and space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,22 +11,19 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = (i + 1); j <= 9; j++)
 		{
 			if (i != j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i <= 9 || j < 8)
-				{
-					if (i == 9 && j == 8)
-						continue;
-					putchar(44);
-					putchar(32);
-				}
+
+				if (i == 8 && j == 9)
+					continue;
+
+				putchar(44);
+				putchar(32);
 			}
-			if (i == 9 && j == 9)
-				break;
 		}
 	}
 	putchar('\n');
