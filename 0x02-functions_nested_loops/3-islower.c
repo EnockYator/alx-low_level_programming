ac699ff3e4b4dc4a2831267a0f8eb5be7d72a1f9
EnockYator <ekyator02@gmail.com>
2023-06-21 01:43:04 -0700

    Added _islower()

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_islower - Checks if a character is a lowercase letter.
+ *@c:The character to be checked.
+ *Return:1 if c is a lowercase letter, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		return (1);
+	}
+}
