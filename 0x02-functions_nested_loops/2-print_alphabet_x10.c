56fcd2de8f22e4b61bfd76ec87e04af13d74a5c7
EnockYator <ekyator02@gmail.com>
2023-06-21 00:12:59 -0700

    Added print_alphabet_x10.c file;

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints alphabet in lowercase
+ *                       ten times followed by newline
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	while (i <= 10)
+	{
+		for (j = 'a'; j <= 'z'; ++j)
+		{
+			_putchar(j);
+		}
+	}
+
+	_putchar('\n');
+	return (0);
+}
+
+
