c40218c7fe1c48090580e98c9ffa2b1d1cd6e1a5
EnockYator <ekyator02@gmail.com>
2023-06-21 00:51:19 -0700

    Added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 
 }
