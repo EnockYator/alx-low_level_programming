aff60cf438dc303680e84e2ca5bece62347b8162
EnockYator <ekyator02@gmail.com>
2023-06-22 11:02:17 -0700

    Added 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(num - '0');
+		_putchar(num + '0');
 	}
 
 	_putchar('\n');
