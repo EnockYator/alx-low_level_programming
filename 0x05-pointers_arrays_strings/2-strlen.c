353c70f450ad4d510f6703ed056f15f18eb114c0
Enock Yator <ekyator02@gmail.com>
2023-06-26 19:04:27 +0300

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,13 @@
 /**
  *_strlen - Print string length
  *@s: String
- *Return: Always 0
+ *Return: Length of the string
  */
 
 int _strlen(char *s)
 {
 int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-_putchar(i + '0');
-}
-}
+
+for (i = 0; s[i] != '\0' ; i++);
+return (i);
+} 
