9b06f67593c647fd83c07364a6e871a5babea90d
Enock Yator <ekyator02@gmail.com>
2023-06-26 19:55:26 +0300

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - Prints a string to stdout
+ *@str: String
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
