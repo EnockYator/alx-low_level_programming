5f219a07fe57c260212e27a8ba50105599b8a487
Enock Yator <ekyator02@gmail.com>
2023-06-30 00:58:48 +0300

    Reversing array elements

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-
-int i = 0;
-
-while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+while ((*s1 == *s2))
 {
-++i;
-return (s1[i] - s2[i]);
+s1++;
+s2++;
+}
+ return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
