d7ad812a2efa76db03a7db1da567753833cc654b
Enock Yator <ekyator02@gmail.com>
2023-06-29 23:24:15 +0300

    function that copies a string from source

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-int j = 0;
 
-dest[i];
-while (n > 0)
+for (; i < n && src[i] != '\0;++i')
 {
-dest[i] = src[j];
-++i;
-++j;
---n;
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i++] = '\0';
 }
 return (dest);
 }
