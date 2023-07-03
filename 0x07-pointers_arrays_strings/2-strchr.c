bd11685c74e61a7d1c381e9346b9c801a22bacb6
Enock Yator <ekyator02@gmail.com>
2023-07-03 16:31:43 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,10 @@ char *_strchr(char *s, char c)
 while (*s != '\0')
 {
 if (*s == c)
-{
-return s;
-}
-else
-{
-return NULL;
-}
+return (s);
+else if (*(s + 1) == c)
+return (s + 1);
 s++;
 }
+return (s + 1);
 }
