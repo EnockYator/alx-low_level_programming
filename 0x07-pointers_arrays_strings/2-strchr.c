6df0bfe3c1eb3337ba5f998e8c7bd91adf2ae9ae
Enock Yator <ekyator02@gmail.com>
2023-07-03 16:52:14 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-{
-if (*s == c)
-return (s);
-else if (*(s + 1) == c)
-return (s + 1);
-s++;
-}
-return (s + 1);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
+
+	return (s + 1);
 }
