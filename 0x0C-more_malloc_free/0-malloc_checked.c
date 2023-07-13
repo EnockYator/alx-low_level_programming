6583f95333b28b76da0d9e009c2bfe8222fc09d5
Enock Yator <ekyator02@gmail.com>
2023-07-13 15:30:28 +0300

    Program that allocates mmemory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ *malloc_checked - Allocates memory using malloc
+ *@b: Integral size of the memory to be allocated
+ *Return: pointer to the allocated memory on success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		fprintf(stderr, "Malloc failed,Terminating with status 98.\n");
+		exit(98);
+	}
+
+	return (mem);
+}
