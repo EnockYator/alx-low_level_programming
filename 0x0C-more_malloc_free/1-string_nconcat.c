f90d8c6905de149776359c74c173a3217f44190c
Enock Yator <ekyator02@gmail.com>
2023-07-14 00:52:21 +0300

    1-string_nconcat - Concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,77 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *_strlen - Finds the length of string
- *@s: First string
- *@ss: Second string
- *Return: Length of the string
+ * _strlen - function that returns the length of a string.
+ * @ss: string.
+ * Return: length of string.
  */
 
-int _strlen(char *s, char *ss)
+int _strlen(char *ss)
 {
-	unsigned int str1;
-	unsigned int str2;
-	unsigned int ls = 0;
-	unsigned int lss = 0;
+	int i;
 
-	while (s[ls] != '\0')
-	{
-		ls++;
-		str1 = ls;
-	}
-	while (ss[lss] != '\0')
-	{
-		lss++;
-		str2 = lss;
-	}
-	return (str1);
-	return (str2);
+	for (i = 0; ss[i] != '\0'; i++)
+		;
+	return (i);
 }
 /**
- *string_nconcat - Concatenates two strings
- *@s1: First string
- *@s2: Second string
- *@n: Number of bytes of the second string
- *Return: NULL on failure, Pointer to newly allocated space on success
+ * string_nconcat - function that concatenates two strings.
+ * @s1: string s1.
+ * @s2: string s2.
+ * @n: number of bytes of s2.
+ * Return: Always 0.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, l1 = 0, l2 = 0;
-	char *_mem;
+	char *str;
+	unsigned int l, t, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	l1 = _strlen(s1, s2);
-	l2 = _strlen(s1, s2);
-	if (n < l2)
-	{
-		l2 = n;
-		_mem = malloc(sizeof(*_mem) * (l1 + n + 2));
 
-		if (_mem == NULL)
-			return (NULL);
-		for (i = 0; i < l1; i++)
-			_mem[i] = s1[i];
-		for (j = 0; j < l2; j++)
-		{
-			_mem[i] = s2[j];
-			i++;
-		}
-	}
-	else
+	t = _strlen(s1);
+	l = _strlen(s2);
+	if (n < l)
+		l = n;
+
+	str = malloc(t + l + 1);
+
+	if (str == NULL)
 	{
-		_mem = malloc(sizeof(*_mem) * (l1 + l2 + 1));
-		for (i = 0; i < l1; i++)
-			_mem[i] = s1[i];
-		for (j = 0; j < l2; j++)
-		{
-			_mem[i] = s2[j];
-			i++;
-		}
+		return (NULL);
 	}
-	_mem[i] = '\0';
-	return (_mem);
+
+	for (i = 0; i < t; i++)
+		*(str + i) = *(s1 + i);
+
+	for (i = 0; i < l; i++)
+		*(str + (i + t)) = *(s2 + i);
+	*(str + (i + t)) = '\0';
+	return (str);
 }
