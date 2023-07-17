fb991a1d089760938e45eeefaa2e0cb18b82ff4c
Enock Yator <ekyator02@gmail.com>
2023-07-17 16:10:45 +0300

    Header file defining a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+
 #ifndef DOG_H
 #define DOG_H
 
@@ -15,8 +16,10 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/**void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
+/*
+ *void print_dog(struct dog *d);
+ *dog_t *new_dog(char *name, float age, char *owner);
+ *void free_dog(dog_t *d);
+*/
 
 	#endif
