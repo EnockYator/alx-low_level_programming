133fc0aef95be6d7daff688b6784c34e93a4930a
Enock Yator <ekyator02@gmail.com>
2023-07-20 13:50:58 +0300

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  *Return: Nothing
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int num;
 	unsigned int i;
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 	for (i = 0; i < n; ++i)
 	{
-		num = var_arg(arguments, int);
+		num = va_arg(arguments, int);
 		if (i > 0 && separator != NULL)
 		{
 			printf("%s", separator);
