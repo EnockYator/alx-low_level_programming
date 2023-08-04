7e93a896f2a71688eb455a182682237d22efe6fd
EnockYator <ekyator02@gmail.com>
2023-08-04 22:38:57 +0300

    Solved directory misnaming

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints binary
+ *
+ * @n: Unsigned int
+ *
+ * Return: Converted number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	(n & 1) ? _putchar('1') : _putchar('0');
+}
